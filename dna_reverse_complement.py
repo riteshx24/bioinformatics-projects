8d4b3682bfe0087415c0d1b200eac114595b1644
riteshx24 <riteshingole25@gmail.com>
2025-05-31 03:54:24 -0700

    Added reverse complement code

--- dna_reverse_complement.py
+++ dna_reverse_complement.py
@@ -1,1 +1,5 @@
-# DNA Reverse Complement Script
+def reverse_complement(seq):
+    complement = {"A":"T", "T":"A", "C":"G", "G":"C"}
+    return "".join(complement.get(base, base) for base in reversed(seq))
+
+print(reverse_complement("ATGC"))
